706867ca79b24adc6c458abe24dd58e612013c19
benny <benny.ahn>
2021-07-07 23:36:05 +0900

    initialize Spring Security Practice Project

---
+++ src/main/java/me/benny/practice/spring/security/SpringSecurityPracticeApplication.java
@@ -0,0 +1,15 @@
+package me.benny.practice.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author benny.ahn
+ */
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+}
