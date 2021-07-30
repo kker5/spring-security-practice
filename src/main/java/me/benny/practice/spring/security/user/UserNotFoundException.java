6697c9e27facd13d7017260678b77f4239022f27
benny <benny.ahn>
2021-07-30 19:41:23 +0900

    Add UserNotFound Exception 추가

---
+++ src/main/java/me/benny/practice/spring/security/user/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package me.benny.practice.spring.security.user;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+        super("유저를 찾을 수 없습니다.");
+    }
+}
