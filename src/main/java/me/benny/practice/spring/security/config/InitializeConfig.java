96fa92536c348417a7b29b65ce3f4a04cc602da6
benny <benny.ahn>
2021-08-08 23:05:11 +0900

    Add 기본 공지사항 내용 추가

--- src/main/java/me/benny/practice/spring/security/config/InitializeConfig.java
+++ src/main/java/me/benny/practice/spring/security/config/InitializeConfig.java
@@ -27,5 +27,6 @@ public class InitializeConfig {
         postService.savePost(user, "테스트3", "테스트3입니다.");
         postService.savePost(user, "여름 여행계획", "여름 여행계획 작성중...");
         noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
+        noticeService.saveNotice("게시글 작성 방법 공지", "1. 회원가입\n2. 로그인\n3. 게시글 작성\n4. 저장\n* 본인 외에는 게시글을 볼 수 없습니다.");
     }
 }
