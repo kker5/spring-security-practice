c33ba26c9c9a7c2782a8902e557ef0ddff73fb4d
benny <benny.ahn>
2021-08-08 23:33:30 +0900

    Modify Application class 주석 수정

--- src/main/java/me/benny/practice/spring/security/SpringSecurityPracticeApplication.java
+++ src/main/java/me/benny/practice/spring/security/SpringSecurityPracticeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author benny.ahn
+ * SpringSecurity 연습 Application
  */
 @SpringBootApplication
 public class SpringSecurityPracticeApplication {
