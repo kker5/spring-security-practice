cf066c9d1943eab23efd8d9bf7ca0cdf8d0edb0b
benny <benny.ahn>
2021-08-13 00:28:48 +0900

    Modify 기본 데이터 초기화 Config refactoring

--- src/main/java/me/benny/practice/spring/security/config/InitializeDefaultConfig.java
+++ src/main/java/me/benny/practice/spring/security/config/InitializeDefaultConfig.java
@@ -1,8 +1,8 @@
 package me.benny.practice.spring.security.config;
 
 import lombok.RequiredArgsConstructor;
-import me.benny.practice.spring.security.notice.NoticeService;
 import me.benny.practice.spring.security.note.NoteService;
+import me.benny.practice.spring.security.notice.NoticeService;
 import me.benny.practice.spring.security.user.User;
 import me.benny.practice.spring.security.user.UserService;
 import org.springframework.context.annotation.Bean;
@@ -14,27 +14,31 @@ import org.springframework.context.annotation.Profile;
  */
 @Configuration
 @RequiredArgsConstructor
-@Profile(value = "!test")
-public class InitializeConfig {
+@Profile(value = "!test") // test 에서는 제외
+public class InitializeDefaultConfig {
 
     private final UserService userService;
     private final NoteService noteService;
     private final NoticeService noticeService;
 
     /**
-     * <h2>유저 등록</h2>
-     * 1. user / user<br/> 2. admin / admin
-     * <h2>노트 등록 4개</h2>
-     * <h2>공지사항 등록 2개</h2>
+     * 유저등록, note 4개 등록
      */
     @Bean
-    public void adminAccount() {
+    public void initializeDefaultUser() {
         User user = userService.signup("user", "user");
-        userService.signupAdmin("admin", "admin");
         noteService.saveNote(user, "테스트", "테스트입니다.");
         noteService.saveNote(user, "테스트2", "테스트2입니다.");
         noteService.saveNote(user, "테스트3", "테스트3입니다.");
         noteService.saveNote(user, "여름 여행계획", "여름 여행계획 작성중...");
+    }
+
+    /**
+     * 어드민등록, 공지사항 2개 등록
+     */
+    @Bean
+    public void initializeDefaultAdmin() {
+        userService.signupAdmin("admin", "admin");
         noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
         noticeService.saveNotice("노트 작성 방법 공지", "1. 회원가입\n2. 로그인\n3. 노트 작성\n4. 저장\n* 본인 외에는 게시글을 볼 수 없습니다.");
     }
