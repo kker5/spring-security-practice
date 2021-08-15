ccee62483ecbbf8fd489aec6634eb9200ed1f80c
kker5 <QpwkZ4i=MDRCwa4vvJH/eW>
2021-08-16 00:47:31 +0900

    Add 정적리소스는 SpringSecurity 대상에서 제외하도록 ignoring추가

--- src/main/java/me/benny/practice/spring/security/config/SpringSecurityConfig.java
+++ src/main/java/me/benny/practice/spring/security/config/SpringSecurityConfig.java
@@ -3,14 +3,18 @@ package me.benny.practice.spring.security.config;
 import lombok.RequiredArgsConstructor;
 import me.benny.practice.spring.security.user.User;
 import me.benny.practice.spring.security.user.UserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -33,23 +37,30 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         http.rememberMe();
         // authorization
         http.authorizeRequests()
-            // /와 /home은 모두에게 허용
-            .antMatchers("/", "/css/**", "/home", "/signup").permitAll()
-            // hello 페이지는 USER 롤을 가진 유저에게만 허용
-            .antMatchers("/note").hasRole("USER")
-            .antMatchers("/admin").hasRole("ADMIN")
-            .antMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
-            .antMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
-            .anyRequest().authenticated();
+                // /와 /home은 모두에게 허용
+                .antMatchers("/", "/home", "/signup").permitAll()
+                // hello 페이지는 USER 롤을 가진 유저에게만 허용
+                .antMatchers("/note").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
+                .anyRequest().authenticated();
         // login
         http.formLogin()
-            .loginPage("/login")
-            .defaultSuccessUrl("/")
-            .permitAll(); // 모두 허용
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll(); // 모두 허용
         // logout
         http.logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-            .logoutSuccessUrl("/");
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // 정적 리소스 spring security 대상에서 제외
+//        web.ignoring().mvcMatchers("/images/**", "/css/**"); // 아래 코드와 같은 코드입니다.
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     /**
