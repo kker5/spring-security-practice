56fa0bd609b61753571e5bdc55eb145555a44101
kker5 <QpwkZ4i=MDRCwa4vvJH/eW>
2021-08-19 22:26:39 +0900

    Modify NoteControllerTest WithUserDetails로 테스트하도록 수정

--- src/test/java/me/benny/practice/spring/security/note/NoteControllerTest.java
+++ src/test/java/me/benny/practice/spring/security/note/NoteControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -39,8 +41,8 @@ class NoteControllerTest {
                 .apply(springSecurity())
                 .alwaysDo(print())
                 .build();
-        user = userRepository.save(new User("user", "user", "ROLE_USER"));
-        admin = userRepository.save(new User("admin", "admin", "ROLE_ADMIN"));
+        user = userRepository.save(new User("user123", "user", "ROLE_USER"));
+        admin = userRepository.save(new User("admin123", "admin", "ROLE_ADMIN"));
     }
 
     @Test
@@ -51,9 +53,15 @@ class NoteControllerTest {
     }
 
     @Test
+    // WithUserDetails 로 테스트 하는 방법
+    @WithUserDetails(
+            value = "user123", // userDetailsService를 통해 가져올 수 있는 유저
+            userDetailsServiceBeanName = "userDetailsService", // UserDetailsService 구현체의 Bean
+            setupBefore = TestExecutionEvent.TEST_EXECUTION // 테스트 실행 직전에 유저를 가져온다.
+    )
     void getNote_인증있음() throws Exception {
         mockMvc.perform(
-                get("/note").with(user(user))
+                get("/note")
         ).andExpect(status().isOk());
     }
 
@@ -69,9 +77,14 @@ class NoteControllerTest {
     }
 
     @Test
+    @WithUserDetails(
+            value = "admin123",
+            userDetailsServiceBeanName = "userDetailsService",
+            setupBefore = TestExecutionEvent.TEST_EXECUTION
+    )
     void postNote_어드민인증있음() throws Exception {
         mockMvc.perform(
-                post("/note").with(csrf()).with(user(admin))
+                post("/note").with(csrf())
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("title", "제목")
                         .param("content", "내용")
@@ -79,9 +92,14 @@ class NoteControllerTest {
     }
 
     @Test
+    @WithUserDetails(
+            value = "user123",
+            userDetailsServiceBeanName = "userDetailsService",
+            setupBefore = TestExecutionEvent.TEST_EXECUTION
+    )
     void postNote_유저인증있음() throws Exception {
         mockMvc.perform(
-                post("/note").with(csrf()).with(user(user))
+                post("/note").with(csrf())
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("title", "제목")
                         .param("content", "내용")
@@ -98,14 +116,24 @@ class NoteControllerTest {
     }
 
     @Test
+    @WithUserDetails(
+            value = "user123",
+            userDetailsServiceBeanName = "userDetailsService",
+            setupBefore = TestExecutionEvent.TEST_EXECUTION
+    )
     void deleteNote_유저인증있음() throws Exception {
         Note note = noteRepository.save(new Note("제목", "내용", user));
         mockMvc.perform(
-                delete("/note?id=" + note.getId()).with(csrf()).with(user(user))
+                delete("/note?id=" + note.getId()).with(csrf())
         ).andExpect(redirectedUrl("note")).andExpect(status().is3xxRedirection());
     }
 
     @Test
+    @WithUserDetails(
+            value = "admin123",
+            userDetailsServiceBeanName = "userDetailsService",
+            setupBefore = TestExecutionEvent.TEST_EXECUTION
+    )
     void deleteNote_어드민인증있음() throws Exception {
         Note note = noteRepository.save(new Note("제목", "내용", user));
         mockMvc.perform(
